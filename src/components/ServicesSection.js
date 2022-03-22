a9082800217b9ea184719634531f684e7f7e1c9d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 19:37:48 +0530

    Create ServicesSection.js

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ServicesSection = () => {
+  return (
+    <div className='services'>
+        <div className='description'>
+            <h2>High <span>quality</span> services</h2>
+        </div>
+    </div>
+  )
+}
+
+export default ServicesSection
