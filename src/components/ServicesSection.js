a5483c214e1b6ac68f84cf3457b5336c1f80ee28
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 20:05:53 +0530

    Update ServicesSection.js

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,4 +1,11 @@
 import React from 'react'
+//Import Icons
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
+
 
 const ServicesSection = () => {
   return (
@@ -7,7 +14,32 @@ const ServicesSection = () => {
             <h2>High <span>quality</span> services</h2>
             <div className='cards'>
                 <div className='card'>
-                    <div className='icon'></div>
+                    <div className='icon'>
+                        <img src={clock} />
+                        <h3>Efficient</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='card'>
+                    <div className='icon'>
+                        <img src={clock} />
+                        <h3>Efficient</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='card'>
+                    <div className='icon'>
+                        <img src={clock} />
+                        <h3>Efficient</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='card'>
+                    <div className='icon'>
+                        <img src={clock} />
+                        <h3>Efficient</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
                 </div>
             </div>
         </div>
