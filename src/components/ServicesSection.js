add8bf3058d45c0253f9bc9df31e3d2fed45f444
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 19:41:35 +0530

    Update ServicesSection.js

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,6 +5,11 @@ const ServicesSection = () => {
     <div className='services'>
         <div className='description'>
             <h2>High <span>quality</span> services</h2>
+            <div className='cards'>
+                <div className='card'>
+                    <div className='icon'></div>
+                </div>
+            </div>
         </div>
     </div>
   )
