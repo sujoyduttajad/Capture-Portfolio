c7632b52ed1277318de40f62e26393766d9b43bd
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 19:08:08 +0530

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -9,12 +9,14 @@ const AboutSection = () => {
                     <h2>We work to make</h2>
                 </div>
                 <div className='hide'>
-                    <h2>We work to make</h2>
+                    <h2>your <span>dreams</span> come</h2>
                 </div>
                 <div className='hide'>
-                    <h2>We work to make</h2>
+                    <h2>true.</h2>
                 </div>
             </div>
+            <p>Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills. </p>
         </div>
     </div>
   )
