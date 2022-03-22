1ab9e5d9405f0b058f9e98239338da44e12ebc1a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 19:05:09 +0530

    Create AboutSection.js

---
+++ src/components/AboutSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <div>
+        <div className='description'>
+            <div className='title'>
+                <div className='hide'>
+                    <h2>We work to make</h2>
+                </div>
+                <div className='hide'>
+                    <h2>We work to make</h2>
+                </div>
+                <div className='hide'>
+                    <h2>We work to make</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutSection
