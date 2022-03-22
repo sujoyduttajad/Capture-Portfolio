dccd6086bf9efdbcc113367dc758494f3ce8daeb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 22:17:13 +0530

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
   return (
     <div>
         <AboutSection/>
+        <ServicesSection/>
     </div>
   )
 }
