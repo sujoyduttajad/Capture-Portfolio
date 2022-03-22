7dc9fdf333c12ee945576f82c120a7faff250b48
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 19:25:41 +0530

    Create AboutUs.js

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutSection from '../components/AboutSection'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <AboutSection/>
+    </div>
+  )
+}
+
+export default AboutUs
