86bcfcfbc7c512eddf6c1373a5e6914cdec57094
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 22:29:51 +0530

    Update FaqSection.js

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -8,6 +8,28 @@ const FaqSection = () => {
             <h4>How Do I Start?</h4>
             <div className='answer'>
                 <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, dolor.</p>
+            </div>
+        </div>
+        <div className='question'>
+            <h4>Daily Schedule</h4>
+            <div className='answer'>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, dolor.</p>
+            </div>
+        </div>
+        <div className='question'>
+            <h4>Different Payment Methods</h4>
+            <div className='answer'>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, dolor.</p>
+            </div>
+        </div>
+        <div className='question'>
+            <h4>What products do you offer?</h4>
+            <div className='answer'>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, dolor.</p>
             </div>
         </div>
     </div>
