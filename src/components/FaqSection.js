22d9ebdba80b88631250cc46726f356ee84529f5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-22 22:25:53 +0530

    Create FaqSection.js

---
+++ src/components/FaqSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+        <h2>Any Questions <span>FAQ</span></h2>
+        <div className='question'>
+            <h4>How Do I Start?</h4>
+            <div className='answer'>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FaqSection
