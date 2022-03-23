2e5ce580426dad43f0c2a1e652c7a27ddf3605d8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-23 15:19:25 +0530

    Update ServicesSection.js

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,14 +6,28 @@ import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
 import { Layout, Description, ImageContainer, Hide } from '../styles'
+import styled from 'styled-components'
 
+const Services = styled(Layout)`
+    h2 {
+        padding-bottom: 5rem;
+    }
+    p {
+        width: 70%;
+        padding: 2rem 0 4rem 0;
+    }
+`;
+const Cards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 const ServicesSection = () => {
   return (
-    <Layout>
+    <Services>
         <Description>
             <h2>High <span>quality</span> services</h2>
-            <div className='cards'>
+            <Cards className='cards'>
                 <div className='card'>
                     <div className='icon'>
                         <img src={clock} alt="icon"/>
@@ -42,12 +56,12 @@ const ServicesSection = () => {
                     </div>
                     <p>Lorem ipsum dolor sit amet.</p>
                 </div>
-            </div>
+            </Cards>
         </Description>
         <div className='image'>
             <img src={home2} alt="" />
         </div>
-    </Layout>
+    </Services>
   )
 }
 
