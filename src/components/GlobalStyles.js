8edcd35a96e57f35976260031043181946f9c676
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-23 11:52:28 +0530

    Update GlobalStyles.js

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -2,17 +2,14 @@ import { createGlobalStyle } from "styled-components";
 
 
 const GlobalStyles = createGlobalStyle`
-
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
-
     body {
         background: #1b1b1b;
     }
-
     button {
         font-weight: bold;
         font-size: 1.1rem;
@@ -22,5 +19,11 @@ const GlobalStyles = createGlobalStyle`
         background: transparent;
         color: white;
         transition: all 0.5s ease;
+        &:hover {
+            background-color: #23d997;
+            color: white;
+        }
     }
 `;
+
+export default GlobalStyles;
