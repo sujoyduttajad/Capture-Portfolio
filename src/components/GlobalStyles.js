1fc008bde7ba343d2b4a4f0b591b6ad90de2d1ad
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-23 13:32:40 +0530

    Update GlobalStyles.js

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -30,10 +30,22 @@ const GlobalStyles = createGlobalStyle`
     h3 {
         color: white;
     }
+    h4 {
+        font-weight: bold;
+        color: #23d997;
+    }
+    span {
+        font-weight: bold;
+        color: #23d997;
+    }
+    a {
+        font-size: 1.1rem;
+    }
     p {
         padding: 3rem 0rem;
         color: #ccc;
         font-size: 1.4rem;
+        line-height: 150%;
     }
 `;
 
