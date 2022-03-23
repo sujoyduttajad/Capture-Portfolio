e0275ea52458e476d80660cdc1be27f88a3ea6cb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-23 15:53:13 +0530

    Update ServicesSection.js

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,7 +5,7 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
-import { Layout, Description, ImageContainer, Hide } from '../styles'
+import { Layout, Description } from '../styles'
 import styled from 'styled-components'
 
 const Services = styled(Layout)`
