1b0d6c79e4a056a6bfa50daef1d7dd1dcc53cae8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-24 20:42:46 +0530

    Create Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Nav = () => {
+  return (
+    <nav>
+        <h1><a href='#'>Capture</a></h1>
+        <ul>
+            <li>
+                <a href='#'>About Us</a>
+            </li>
+            <li>
+                <a href='#'>Our Work</a>
+            </li>
+            <li>
+                <a href='#'>Contact Us</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
