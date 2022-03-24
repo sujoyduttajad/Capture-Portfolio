3a7a279c139450ad23cd8ed71a4b23dceabd8434
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-24 22:23:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import GlobalStyles from './components/GlobalStyles';
-import Nav from './components/Nav';
+import React from "react";
+import { Route } from "react-router-dom";
+import GlobalStyles from "./components/GlobalStyles";
+import Nav from "./components/Nav";
 // Import pages down here
-import AboutUs from './pages/AboutUs';
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 
 function App() {
   return (
     <div>
-      <GlobalStyles/>
-      <Nav/>
-      <AboutUs/>
+      <GlobalStyles />
+      <Nav />
+      <Route path="/">
+        <AboutUs />
+      </Route>
+      <Route path="/work">
+        <ContactUs />
+      </Route>
+      <Route path="/contact">
+        <OurWork />
+      </Route>
     </div>
   );
 }
