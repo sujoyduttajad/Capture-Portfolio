cedd0f91c6e171537d6fbb1627fa4b5c89dbd68f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-05 18:03:13 +0530

    Update OurWork.js

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+//Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
+const Work = styled.div`
+  min-height: 100vh;
+`;
 const OurWork = () => {
   return (
-    <div><h1>OurWork</h1></div>
-  )
-}
+    <Work>
+      <h1>OurWork</h1>
+    </Work>
+  );
+};
 
-export default OurWork
+export default OurWork;
