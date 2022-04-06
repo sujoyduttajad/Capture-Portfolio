1a34740ac40824d2cfc5fe9f13be11c7e12f10e9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-06 12:31:45 +0530

    Update MovieDetail.js

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from "styled-components";
 import { useHistory } from 'react-router-dom';
 import { MovieState } from '../movieState';
@@ -7,7 +7,13 @@ const MovieDetail = () => {
     const history =  useHistory();
     const url = history.location.pathname;
 
-    const [movies, setMovies] = useState(MovieState)
+    const [movies, setMovies] = useState(MovieState);
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+        
+    }, [])
 
   return (
     <div>MovieDetail</div>
