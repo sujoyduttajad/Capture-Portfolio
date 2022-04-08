6bc9286c2d1c31405a5049a2abca65e5b73bd38e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-08 13:12:12 +0530

    Update MovieDetail.js

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -3,9 +3,21 @@ import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { MovieState } from "../movieState";
 
-const Details = styled.div``;
+const Details = styled.div`
+  color: #fff;
+`;
 
-const Headline = styled.div``;
+const Headline = styled.div`
+  min-height: 90vh;
+  padding-top: 20vh;
+  position: relative;
+  h2 {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+  }
+`;
 
 const MovieDetail = () => {
   const history = useHistory();
