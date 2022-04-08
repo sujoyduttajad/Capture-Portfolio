95c269cd03d7c45409e20279fdbe00e72412390f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-08 12:15:12 +0530

    Update MovieDetail.js

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,23 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { useHistory } from 'react-router-dom';
-import { MovieState } from '../movieState';
+import { useHistory } from "react-router-dom";
+import { MovieState } from "../movieState";
+
+const Details = styled.div``;
+
+const Headline = styled.div``;
 
 const MovieDetail = () => {
-    const history =  useHistory();
-    const url = history.location.pathname;
+  const history = useHistory();
+  const url = history.location.pathname;
 
-    const [movies, setMovies] = useState(MovieState);
-    const [movie, setMovie] = useState(null);
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
 
-    useEffect(() => {
-        const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
-        setMovie(currentMovie)
-    }, [movies, url])
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie);
+  }, [movies, url]);
 
   return (
-    <div>MovieDetail</div>
-  )
-}
+    <Details>
+      {movie ? (
+        <Headline>
+          <h2>{movie.title}</h2>
+          <img src={movie.mainImg} alt="movie" />
+        </Headline>
+      ) : (
+        <h3>Sorry the movie you are looking is not available</h3>
+      )}
+    </Details>
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;
