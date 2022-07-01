fca820bfd121f92846561d9c2d0ec85e183efcfe
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-07-01 15:31:59 +0530

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,14 +2,16 @@ import React from 'react'
 import AboutSection from '../components/AboutSection'
 import FaqSection from '../components/FaqSection'
 import ServicesSection from '../components/ServicesSection'
+import { motion } from "framer-motion"
+import { pageAnimation } from '../animation'
 
 const AboutUs = () => {
   return (
-    <div>
+    <motion.div variants={pageAnimation}>
         <AboutSection/>
         <ServicesSection/>
         <FaqSection/>
-    </div>
+    </motion.div>
   )
 }
 
