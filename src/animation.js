e51b2fb12143b54209f8bd7fad3f1c3e65349e76
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-07-01 13:53:46 +0530

    Create animation.js

---
+++ src/animation.js
@@ -0,0 +1,13 @@
+export const pageAnimation = {
+    hidden: {
+        opacity: 0,
+        y: 300,
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+        },
+    },
+};
