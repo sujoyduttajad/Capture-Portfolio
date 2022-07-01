a91ad1263578967504207661c0d8b01ea53d1135
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-07-01 13:25:39 +0530

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,35 +1,39 @@
-import React from 'react'
+import React from "react";
 import home1 from "../img/home1.png";
-import styled from 'styled-components'
-import { Layout, Description, ImageContainer, Hide } from '../styles'
-
+import { Layout, Description, ImageContainer, Hide } from "../styles";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
   return (
     <Layout>
-        <Description>
-            <div className='title'>
-                <Hide>
-                    <h2>We work to make</h2>
-                </Hide>
-                <Hide>
-                    <h2>your <span>dreams</span> come</h2>
-                </Hide>
-                <Hide>
-                    <h2>true.</h2>
-                </Hide>
-            </div>
-            <p>
-                Contact us for any photography or videography ideas that you have. We
-                have professionals with amazing skills. 
-            </p>
-            <button>Contact Us</button>
-        </Description>
-        <ImageContainer>
-            <img src={home1} alt="guy with a camera" />
-        </ImageContainer>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <motion.h2 
+                animate={{ opacity: 1, transition: {duration: 2} }}
+                initial={{ opacity: 0 }}
+            >
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <ImageContainer>
+        <img src={home1} alt="guy with a camera" />
+      </ImageContainer>
     </Layout>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
