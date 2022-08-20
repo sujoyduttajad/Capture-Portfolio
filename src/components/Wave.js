ef9f0b10e570fab575ae37653c0a45e1e7b232b6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 21:57:06 +0530

    Update Wave.js

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -17,11 +17,11 @@ const Wave = () => {
     >
       <motion.path
         initial={{ pathLength: 0, pathOffset: 1 }}
-        animation={{ pathLength: 1, pathOffset: 0 }}
+        animate={{ pathLength: 1, pathOffset: 0 }}
         transition={{ duration: 1 }}
         d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5"
         stroke="#D96ED4"
-        strokeOpacity="0.1"
+        strokeOpacity="0.4"
         strokeWidth="10"
       />
     </WaveSvg>
