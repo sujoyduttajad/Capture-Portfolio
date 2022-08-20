7c6210a7556e0d5da31bda1780f0293714b805b2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 21:37:32 +0530

    Fixed non-styled-link styles

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -34,31 +34,31 @@ const Movie = styled.div`
 const OurWork = () => {
   return (
     <Work
-      variants={pageAnimation} 
-      initial="hidden" 
+      variants={pageAnimation}
+      initial="hidden"
       animate="show"
       exit="exit"
-      style={{ backgroundColor: '#FCFCFC' }}
+      style={{ backgroundColor: "#FCFCFC" }}
     >
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
-        <Link to="/work/the-athlete">
-          <img src={athlete} alt='athlete' />
+        <Link className="non-styled-link" to="/work/the-athlete">
+          <img src={athlete} alt="athlete" />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className="line"></div>
-        <Link to="/work/the-racer">
-          <img src={theracer} alt='theracer' />
+        <Link className="non-styled-link" to="/work/the-racer">
+          <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
       <Movie>
         <h2>Good Times</h2>
         <div className="line"></div>
-        <Link to="/work/good-times">
-          <img src={goodtimes} alt='goodtimes' />
+        <Link className="non-styled-link" to="/work/good-times">
+          <img src={goodtimes} alt="goodtimes" />
         </Link>
       </Movie>
     </Work>
