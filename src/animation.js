7a52fe9a3326874922561191f8017d15383556a4
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 18:46:05 +0530

    Update animation.js

--- src/animation.js
+++ src/animation.js
@@ -1,20 +1,29 @@
 export const pageAnimation = {
-    hidden: {
-        opacity: 0,
-        y: 300,
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
     },
-    show: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 1,
-        },
+  },
+  exit: {
+    opacity: 0,
+    y: 300,
+    transition: {
+      duration: 1,
     },
-    exit: {
-        opacity: 0,
-        y: 300,
-        transition: {
-            duration: 1,
-        }
-    }
+  },
+};
+
+export const titleAnimation = {
+  hidden: {
+    y: 200,
+  },
+  show: {
+    y: 0,
+  },
 };
