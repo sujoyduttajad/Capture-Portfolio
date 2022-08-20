79efd0e9c7221577eb3ca4f242e71e04b4054235
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 21:20:52 +0530

    Create Wave.js

---
+++ src/components/Wave.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+
+const Wave = () => {
+  return (
+    <div>Wave</div>
+  )
+}
+
+export default Wave
