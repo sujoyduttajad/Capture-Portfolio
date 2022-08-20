a5db563f68a79202097f2194889b907973cead22
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 17:08:18 +0530

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import AboutSection from '../components/AboutSection'
-import FaqSection from '../components/FaqSection'
-import ServicesSection from '../components/ServicesSection'
-import { motion } from "framer-motion"
-import { pageAnimation } from '../animation'
+import React from "react";
+import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
+// Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const AboutUs = () => {
   return (
-    <motion.div variants={pageAnimation}>
-        <AboutSection/>
-        <ServicesSection/>
-        <FaqSection/>
+    <motion.div 
+      variants={pageAnimation} 
+      initial="hidden" 
+      animate="show"
+    >
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
     </motion.div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
