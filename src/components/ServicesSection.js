9aa59266af6fb4427b12299c3efbc81534abe98c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 18:52:42 +0530

    Update ServicesSection.js

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,81 +1,83 @@
-import React from 'react'
+import React from "react";
 //Import Icons
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
-import { Layout, Description } from '../styles'
-import styled from 'styled-components'
+import { Layout, Description } from "../styles";
+import styled from "styled-components";
 
 const Services = styled(Layout)`
-    h2 {
-        padding-bottom: 5rem;
-    }
-    p {
-        width: 70%;
-        padding: 2rem 0 4rem 0;
-    }
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0 4rem 0;
+  }
 `;
 const Cards = styled.div`
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 `;
 const Card = styled.div`
-    flex-basis: 20rem;
-    .icon {
-        display: flex;
-        align-items: center;
-    }
-    h3 {
-        margin-left: 1rem;
-        background: white;
-        color: #333;
-        padding: 1rem;
-    }
-`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+  }
+  h3 {
+    margin-left: 1rem;
+    background: white;
+    color: #333;
+    padding: 1rem;
+  }
+`;
 
 const ServicesSection = () => {
   return (
     <Services>
-        <Description>
-            <h2>High <span>quality</span> services</h2>
-            <Cards className='cards'>
-                <Card className='card'>
-                    <div className='icon'>
-                        <img src={clock} alt="icon"/>
-                        <h3>Efficient</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                <Card className='card'>
-                    <div className='icon'>
-                        <img src={teamwork} alt="icon"/>
-                        <h3>Teamwork</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                <Card className='card'>
-                    <div className='icon'>
-                        <img src={diaphragm} alt="icon"/>
-                        <h3>Diaphragm</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                <Card className='card'>
-                    <div className='icon'>
-                        <img src={money} alt="icon"/>
-                        <h3>Affordable</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-            </Cards>
-        </Description>
-        <div className='image'>
-            <img src={home2} alt="" />
-        </div>
+      <Description>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <Cards className="cards">
+          <Card className="card">
+            <div className="icon">
+              <img src={clock} alt="icon" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card className="card">
+            <div className="icon">
+              <img src={teamwork} alt="icon" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="icon" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card className="card">
+            <div className="icon">
+              <img src={money} alt="icon" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <div className="image">
+        <img src={home2} alt="" />
+      </div>
     </Services>
-  )
-}
+  );
+};
 
-export default ServicesSection
+export default ServicesSection;
