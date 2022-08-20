9b2f52a0dd28672b72258384dc45f203e9c2ed41
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-20 21:51:48 +0530

    Update Wave.js

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -3,22 +3,22 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const WaveSvg = styled.svg`
-    position: absolute;
-    left: 0;
-    z-index: 1;
+  position: absolute;
+  left: 0;
+  z-index: 1;
 `;
-
 
 const Wave = () => {
   return (
     <WaveSvg
-      width="1440"
-      height="363"
       viewBox="0 0 1440 363"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
+      <motion.path
+        initial={{ pathLength: 0, pathOffset: 1 }}
+        animation={{ pathLength: 1, pathOffset: 0 }}
+        transition={{ duration: 1 }}
         d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5"
         stroke="#D96ED4"
         strokeOpacity="0.1"
