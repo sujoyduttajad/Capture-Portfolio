6e41b92e53dce7d5840cc9f7acced41cea50575c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-21 20:26:38 +0530

    Update animation.js

--- src/animation.js
+++ src/animation.js
@@ -81,5 +81,11 @@ export const slider = {
 
 export const sliderContainer = {
   hidden: { opacity: 1 },
-  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+      ease: "easeOut",
+    },
+  },
 };
