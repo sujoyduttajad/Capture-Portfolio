f06cb9e8ca712c9447740c74d8e2b20d9e674aa5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-24 14:32:11 +0530

    Update Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 const Toggle = ({ children }) => {
-
+    console.log(children)
   const [toggle, setToggle] = useState(false);
-  
+
   return (
     <div onClick={() => setToggle(!toggle)}>
         { toggle ? children : '' }
