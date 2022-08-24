e37f28ed4b9b50765e754a24e219a18758ae5a56
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-25 02:03:47 +0530

    Update Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 
-const Toggle = ({ children }) => {
+const Toggle = ({ children, title }) => {
     console.log(children)
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   return (
     <div onClick={() => setToggle(!toggle)}>
+        <motion.h4>{title}</motion.h4>
         { toggle ? children : '' }
+        <div className="faq-line"></div>
     </div>
   )
 };
