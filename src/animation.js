60b139794be0e561fd7648e78eccf62ca86a6db3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-27 14:39:03 +0530

    Update animation.js

--- src/animation.js
+++ src/animation.js
@@ -90,6 +90,23 @@ export const sliderContainer = {
   },
 };
 
+export const scrollReveal = {
+  hidden: {
+    opacity: 0,
+    scale: 1.2,
+    transition: {
+      duration: 0.5,
+    }
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5
+    }
+  }
+}
+
 // Variants for rotating SVG
 export const rotateSvg = {
   hidden: {
