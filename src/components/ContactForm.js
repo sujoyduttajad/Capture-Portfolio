63ef2772185e2c3f59f182aed95ec8d65b847728
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-28 19:04:37 +0530

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,10 +11,12 @@ const ContactStyle = styled(motion.section)`
   height: 100%;
   display: flex;
   flex-direction: row;
+  justify-content: space-around;
   color: #1b1b1b;
   background-color: #23d997;
 `;
 const FormTitle = styled(motion.div)`
+width: 40%;
   h4 {
     color: #1b1b1b;
     font-size: 4em;
@@ -26,20 +28,44 @@ const FormTitle = styled(motion.div)`
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
+  width: 60%;
+  max-width: 30rem;
+  width: 100%;
   padding: 3rem;
   border-radius: 7px;
   background-color: #1b1b1b;
   label {
     color: #23d997;
+    margin-top: 1rem;
+    margin-bottom: 0.2rem;
   }
   button {
     background: #23d997;
     color: #1b1b1b;
     padding: 0.8rem 1rem;
-    border-radius: 7px;
+    border-radius: 5.5px;
     margin-top: 2rem;
   }
-  input {
+  input, textarea {
+    font-family: inherit;
+    padding: 10px 20px;
+    border-radius: 5.55px;
+    background-color: #f7f7f7;
+    outline: none;
+    border: none;
+    font-size: 16px;
+    flex-grow: 2;
+    border: 1px solid #fff;
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      color: #242424;
+    }
+    @media screen and (max-width: 820px) {
+      width: 100%;
+      margin: 0 0 16px 0;
+    }
   }
 `;
 
