f0dc35eade21bf1512d79a2c4f6627a44a3a7a34
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-30 13:11:32 +0530

    Update ScrollTop.js

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -3,4 +3,13 @@ import { useLocation } from "react-router-dom";
 
 const ScrollTop = () => {
     const { pathname } = useLocation();
+    useEffect(() => {
+        window.scroll({
+            top: 0,
+            left: 0,
+        });
+    }, [pathname]);
+    return null;
 }
+
+export default ScrollTop;
