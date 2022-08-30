30903f7e61d1e6889244cd3e32301b9c88d7d4ad
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-30 13:24:20 +0530

    Update ContactUs.js

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import ContactForm from "../components/ContactForm";
 import ContactSocial from "../components/ContactSocial";
+import ScrollTop from "../components/ScrollTop";
 
 const ContactUs = () => {
   return (
     <>
       <ContactForm />
       <ContactSocial />
+      <ScrollTop />
     </>
   );
 };
