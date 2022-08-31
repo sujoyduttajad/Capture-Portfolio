6ae58a4a970df00690e96751c316601fc12c6ff3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-08-31 18:39:02 +0530

    Added a Video component

--- src/components/OurSuccess.js
+++ src/components/OurSuccess.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
+// Animations
+import { motion } from "framer-motion";
+
+
+const VideoContainer = styled.div``;
+const Video = styled(motion.video)``;
 
 const OurSuccess = () => {
   return (
-    <div>OurSuccess</div>
-  )
-}
+    <VideoContainer>
+      <Video controls width="100%" >
+        <source
+          src="https://player.vimeo.com/external/524033439.hd.mp4?s=22f1a7abade536bc83845b40d3181c97b8229d8d&profile_id=175"
+          type="video/mp4"
+        />
+        Sorry, your browser doesn't support embedded videos.
+      </Video>
+    </VideoContainer>
+  );
+};
 
-export default OurSuccess
+export default OurSuccess;
