5fc7e88e831e1aad174fd7530b1e95c72d11b9c3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-03 13:37:58 +0530

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,11 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import FaqSection from "../components/FaqSection";
 import ServicesSection from "../components/ServicesSection";
+import OurSuccess from "../components/OurSuccess";
+import ScrollTop from "../components/ScrollTop";
 // Animations
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
-import ScrollTop from "../components/ScrollTop";
-import OurSuccess from "../components/OurSuccess";
 
 const AboutUs = () => {
   return (
