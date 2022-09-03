7243836b2ca945599f28afc2e902cab018a3be4a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-03 15:35:46 +0530

    Create SubsAndPacks.js

---
+++ src/components/SubsAndPacks.js
@@ -0,0 +1,168 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const PricingSection = styled.div`
+  padding: 100px 0 160px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #4b59f7;
+`;
+
+export const PricingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  @media screen and (max-width: 960px) {
+    margin: 0 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const PricingHeading = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  margin-bottom: 24px;
+`;
+
+export const PricingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+`;
+
+export const PricingCard = styled(motion.div)`
+  background: #242424;
+  box-shadow: 0 6px 20px rgba(56, 125, 255, 0.2);
+  width: 280px;
+  height: 500px;
+  text-decoration: none;
+  border-radius: 4px;
+  &:nth-child(2) {
+    margin: 24px;
+  }
+  &:hover {
+    transform: scale(1.06);
+    transition: all 0.3s ease-out;
+    color: #1c2237;
+  }
+  @media screen and (max-width: 960px) {
+    width: 90%;
+    &:hover {
+      transform: none;
+    }
+  }
+`;
+
+export const PricingCardInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  padding: 24px;
+  align-items: center;
+  color: #fff;
+`;
+
+export const PricingCardIcon = styled.div`
+  margin: 24px 0;
+`;
+
+export const PricingCardPlan = styled.h3`
+  margin-bottom: 5px;
+  font-size: 24px;
+`;
+
+export const PricingCardCost = styled.h4`
+  font-size: 40px;
+`;
+
+export const PricingCardLength = styled.p`
+  font-size: 14px;
+  margin-bottom: 24px;
+`;
+
+export const PricingCardFeatures = styled.ul`
+  margin: 16px 0 32px;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #a9b3c1;
+`;
+
+export const PricingCardFeature = styled.li`
+  margin-bottom: 10px;
+`;
+const SubsAndPacks = () => {
+  return (
+    <PricingSection>
+      <PricingWrapper>
+        <PricingHeading>Our Services</PricingHeading>
+        <PricingContainer>
+          <PricingCard to="/sign-up">
+            <PricingCardInfo>
+              <PricingCardIcon>
+                {/* <GiRock /> */}
+              </PricingCardIcon>
+              <PricingCardPlan>Starter Pack</PricingCardPlan>
+              <PricingCardCost>$99.99</PricingCardCost>
+              <PricingCardLength>per month</PricingCardLength>
+              <PricingCardFeatures>
+                <PricingCardFeature>100 New Users</PricingCardFeature>
+                <PricingCardFeature>$10,000 Budget</PricingCardFeature>
+                <PricingCardFeature>Retargeting analytics</PricingCardFeature>
+              </PricingCardFeatures>
+              <button primary>Choose Plan</button>
+            </PricingCardInfo>
+          </PricingCard>
+          <PricingCard to="/sign-up">
+            <PricingCardInfo>
+              <PricingCardIcon>
+                {/* <GiCrystalBars /> */}
+              </PricingCardIcon>
+              <PricingCardPlan>Gold Rush</PricingCardPlan>
+              <PricingCardCost>$299.99</PricingCardCost>
+              <PricingCardLength>per month</PricingCardLength>
+              <PricingCardFeatures>
+                <PricingCardFeature>1000 New Users</PricingCardFeature>
+                <PricingCardFeature>$50,000 Budget</PricingCardFeature>
+                <PricingCardFeature>Lead Gen Analytics</PricingCardFeature>
+              </PricingCardFeatures>
+              <button primary>Choose Plan</button>
+            </PricingCardInfo>
+          </PricingCard>
+          <PricingCard to="/sign-up">
+            <PricingCardInfo>
+              <PricingCardIcon>
+                {/* <GiCutDiamond /> */}
+              </PricingCardIcon>
+              <PricingCardPlan>Diamond Kings</PricingCardPlan>
+              <PricingCardCost>$999.99</PricingCardCost>
+              <PricingCardLength>per month</PricingCardLength>
+              <PricingCardFeatures>
+                <PricingCardFeature>Unlimited Users</PricingCardFeature>
+                <PricingCardFeature>Unlimited Budget</PricingCardFeature>
+                <PricingCardFeature>24/7 Support</PricingCardFeature>
+              </PricingCardFeatures>
+              <button primary>Choose Plan</button>
+            </PricingCardInfo>
+          </PricingCard>
+        </PricingContainer>
+      </PricingWrapper>
+    </PricingSection>
+  );
+};
+
+export default SubsAndPacks;
